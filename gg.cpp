08a9137358a3f40d519215a17e9b29352ab34ef9
Sam <ssambhavv@gmail.com>
2022-04-03 14:42:08 -0700

    GG

---
+++ gg.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    string name = "Sam";
+    int age;
+    age = 19;
+    cout << "My name is " << name << " and I am "  << age << " years old." << endl;
+
+}
